f0b916c595f9bf890c33564b80dbd3ed88c41cd1
Alielson Ferreira Pequeno <alielson_pequeno@hotmail.com>
2024-06-13 16:43:33 -0300

    Add new products

--- tests/factories.py
+++ tests/factories.py
@@ -12,6 +12,9 @@ def products_data():
         {"name": "Iphone 11 Pro Max", "quantity": 20, "price": "4.500", "status": True},
         {"name": "Iphone 12 Pro Max", "quantity": 15, "price": "5.500", "status": True},
         {"name": "Iphone 13 Pro Max", "quantity": 5, "price": "6.500", "status": True},
+        {"name": "Xiaomi 12 Ultra", "quantity": 25, "price": "4.700", "status": True},
+        {"name": "PocoPhone 13 Ultra", "quantity": 18, "price": "5.600", "status": True},
+        {"name": "Samsung 14 Ultra", "quantity": 7, "price": "6.800", "status": True},
         {
             "name": "Iphone 15 Pro Max",
             "quantity": 3,
@@ -19,3 +22,4 @@ def products_data():
             "status": False,
         },
     ]
+
